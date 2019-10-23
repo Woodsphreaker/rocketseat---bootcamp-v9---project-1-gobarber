9a7dfa47d771c01b8065e499edf9ec09977af94f
Carlo Enrico <carlo.enrico@uol.com.br>
2019-10-23 00:21:07 -0300

    Initial commit

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import { Router } from 'express'
+
 const router = Router()
 
-router.get('/', (req, res) => {
-  return res.send({message: "ok, running"})
-})
+router.get('/', (req, res) => res.send({ message: 'ok, running' }))
 
 export default router
