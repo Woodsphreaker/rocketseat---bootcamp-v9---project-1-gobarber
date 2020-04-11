cc6585e49787dcb512ad27224c32531bbbe2bddd
Carlo Enrico <carlo.enrico@uol.com.br>
2020-04-11 00:37:29 -0300

    feat: added unform

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
+
 import Logo from '~/assets/img/logo.svg'
 
 export default function SignIn() {
+  const handleSubmit = (data) => {
+    console.tron.log(data)
+  }
+
   return (
     <>
       <img alt="GoBarber" src={Logo} />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Acessar</button>
         <Link to="/register">Não tenho cadastro</Link>
-      </form>
+      </Form>
     </>
   )
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
+
 import Logo from '~/assets/img/logo.svg'
 
 export default function SignUp() {
+  const handleSubmit = (data) => {
+    console.tron.log(data)
+  }
+
   return (
     <>
       <img alt="GoBarber" src={Logo} />
-      <form>
-        <input type="text" placeholder="Seu nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="nome" type="text" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Acessar</button>
         <Link to="/">Já tenho cadastro</Link>
-      </form>
+      </Form>
     </>
   )
 }
