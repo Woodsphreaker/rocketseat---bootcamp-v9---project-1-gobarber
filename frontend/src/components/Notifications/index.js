d6e090545e943105befedf26bec0acab73cc719f
Carlo Enrico <carlo.enrico@uol.com.br>
2020-04-21 02:37:09 -0300

    feat: added date-fns to calculate time between notification registry and current date

--- frontend/src/components/Notifications/index.js
+++ frontend/src/components/Notifications/index.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdNotifications } from 'react-icons/md'
+import { parseISO, formatDistance } from 'date-fns'
+import pt from 'date-fns/locale/pt-BR'
+
+import api from '~/services/api'
 
 import {
   Container,
@@ -10,21 +14,77 @@ import {
 } from './styles'
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false)
+  const [notifications, setNotifications] = useState([])
+
+  const handleToggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  const prepareData = (notifications) => {
+    return notifications.map((notification) => ({
+      ...notification,
+      timeDistance: formatDistance(
+        parseISO(notification.createdAt),
+        new Date(),
+        { addSuffix: true, locale: pt }
+      ),
+    }))
+  }
+
+  const getNotifications = async () => {
+    try {
+      const { data } = await api.get('notifications')
+      const formatedData = prepareData(data)
+      setNotifications(formatedData)
+    } catch (error) {
+      console.tron.error(error)
+    }
+  }
+
+  const handleNotificationRead = async (notificationID) => {
+    try {
+      await api.put(`notifications/${notificationID}`)
+      getNotifications()
+    } catch (error) {
+      console.tron.errror(error)
+    }
+  }
+
+  useEffect(() => {
+    getNotifications()
+  }, [])
+
+  console.log(notifications)
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge onClick={handleToggleVisible} hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          {[...Array(20)].map((el, i) => (
-            <Notification unread={i === 0 || false} key={`notification-${el}`}>
-              <p>Você tem um novo agendamento para amanhã</p>
-              <time>há 2 dias</time>
-              <button type="button">Marcar como lida</button>
-            </Notification>
-          ))}
+          {notifications.map((el) => {
+            const { _id: notificationID, read, content, timeDistance } = el
+            return (
+              <Notification
+                unread={!read}
+                key={`notification-${notificationID}`}
+              >
+                <p>{content}</p>
+                <time>{timeDistance}</time>
+                {!read && (
+                  <button
+                    type="button"
+                    onClick={() => handleNotificationRead(notificationID)}
+                  >
+                    Marcar como lida
+                  </button>
+                )}
+              </Notification>
+            )
+          })}
         </Scroll>
       </NotificationList>
     </Container>
