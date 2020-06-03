a31254261cf2acb894a758868fa841fe313a1142
Carlo Enrico <carlo.enrico@uol.com.br>
2020-06-03 01:36:21 -0300

    feat: added schedules list

--- mobile/src/pages/dashboard/index.js
+++ mobile/src/pages/dashboard/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
-// import { Container } from './styles';
+import React, { useState } from 'react'
 
 import Background from '~/components/LinearGradient'
+import Apointments from '~/components/Apointments'
+
+import { Container, Title, List } from './styles'
 
 const dashboard = (props) => {
   const { navigation } = props
 
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
   return (
     <Background colors={['#7159c1', '#ab59c1']} flexSize={1}>
-      <View>
-        <Text>Dash</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text>Goto Login</Text>
-        </TouchableOpacity>
-      </View>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={(item) => String(item)}
+          renderItem={({ item }) => <Apointments apointment={item} />}
+        />
+      </Container>
     </Background>
   )
 }
